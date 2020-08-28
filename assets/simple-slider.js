e1179667402c6b0fa9234d9e4efa0d91814ec678
Amber Alter <aarnold@Ambers-MacBook-Air.local>
2020-08-28 15:49:33 -0700

    Adjust slider settings and frontend styles

--- assets/simple-slider.js
+++ assets/simple-slider.js
@@ -1,5 +1,14 @@
+/**
+ * Adjust slider settings in this file
+ * Tiny Slider 2 documentation: https://github.com/ganlanyuan/tiny-slider
+ */
+
 window.onload = function() {
 	const sliders = document.querySelectorAll( '.aa-simple-slider' );
+	const controlArrows = [
+		'&#x027EA;', //left arrow
+		'&#x027EB;', //right arrow
+	];
 
 	sliders.forEach( function( slider ) {
 		// eslint-disable-next-line no-unused-vars, no-undef
@@ -7,7 +16,10 @@ window.onload = function() {
 			container: slider,
 			items: 1,
 			slideBy: 'page',
-			autoplay: true,
+			controlsPosition: 'bottom',
+			navPosition: 'bottom',
+			controlsText: controlArrows,
+			autoHeight: 'true',
 		} );
 	} );
 };
